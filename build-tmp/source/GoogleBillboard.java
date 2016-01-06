79c95d694a0064ca5d437ea0e85a6f0018c0ba97
nicolethai <ThaiNicole@gmail.com>
2016-01-06 11:36:21 -0800

    Init Commit. #6

--- build-tmp/source/GoogleBillboard.java
+++ build-tmp/source/GoogleBillboard.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class GoogleBillboard extends PApplet {
+
 public final static String e = "2.718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
 
 public void setup()  
@@ -15,7 +31,6 @@ public void setup()
 public void draw()  
 {   
 	//not needed for this assignment
-	// 6. Add a loop to `draw()` that moves through the digits of e until it finds the first 10 digit prime.
 }  
 public boolean isPrime(double dNum)  
 {   
@@ -30,3 +45,12 @@ public boolean isPrime(double dNum)
 	}   
     return true;  
 } 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "GoogleBillboard" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
